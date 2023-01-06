7008dd62a3ca9d53d8b895668900462d3182dfa5
Jawahar B <90580495+JawaharBalu@users.noreply.github.com>
2023-01-06 10:53:31 -0500

    Added funtionality to the input field

--- src/components/inputField/InputField.jsx
+++ src/components/inputField/InputField.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import './InputField.css'
+import React from 'react';
+import './InputField.css';
+import { useState } from 'react';
+
 
 const InputField = () => {
+  const  [inputString,setInputString] = useState('');
+  const [addString, setAddString] = useState([]);
+  const handleChange = (event) => {
+     setInputString(event.target.value);
+   
+  }
+  const handleClick = (event)=>{
+   setAddString([...addString,inputString]);
+    console.log(addString)
+
+  }
+
   return (
     <div>
-        <label>Enter your Task</label>
-        <input type ='text'></input>
-        <button>Add</button>
+        {/* <label htmlFor='input'>Enter your Task</label> */}
+        <input onChange={handleChange} placeholder='Enter your Task' type ='text'></input>
+        <button onClick={handleClick} >Add</button>
+        {addString.map((elem, index)=>{
+          return [<h2 key={index}>{elem}</h2>,
+                  <h2>{elem}</h2>]
+        } 
+
+        )}
     </div>
 
   )
